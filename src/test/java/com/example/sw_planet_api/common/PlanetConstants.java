97959e246de7cad1db01b0de44b6820979470ac1
Lucas Silva <lucassdeoliveira@outlook.com.br>
2025-11-20 10:26:57 -0300

    feat: create constants to use on service tests

--- src/test/java/com/example/sw_planet_api/common/PlanetConstants.java
+++ src/test/java/com/example/sw_planet_api/common/PlanetConstants.java
@@ -4,4 +4,6 @@ import com.example.sw_planet_api.domain.Planet;
 
 public class PlanetConstants {
     public static final Planet PLANET = new Planet("name", "climate", "terrain");
+    public static final Planet INVALID_PLANET = new Planet("", "", "");
+
 }
