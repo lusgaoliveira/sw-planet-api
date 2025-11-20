8dbd00d01809d6730245b23a3e1813f5a92752f4
Lucas Silva <lucassdeoliveira@outlook.com.br>
2025-11-20 10:24:33 -0300

    feat: add query builder class that uses example api to make querys

---
+++ src/main/java/com/example/sw_planet_api/domain/QueryBuilder.java
@@ -0,0 +1,14 @@
+package com.example.sw_planet_api.domain;
+
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+
+public class QueryBuilder {
+    public static Example<Planet> makeQuery(Planet planet) {
+        ExampleMatcher matcher =  ExampleMatcher
+                .matchingAll()
+                .withIgnoreCase()
+                .withIgnoreNullValues();
+        return Example.of(planet, matcher);
+    }
+}
