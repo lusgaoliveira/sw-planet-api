26933be3f9fa718590bd2f14afbb9efaf018c384
Lucas Silva <lucassdeoliveira@outlook.com.br>
2025-12-15 21:48:21 -0300

    refactor: create a private constructor for class don´t count in tests

--- src/main/java/com/example/sw_planet_api/domain/QueryBuilder.java
+++ src/main/java/com/example/sw_planet_api/domain/QueryBuilder.java
@@ -4,11 +4,10 @@ import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 
 public class QueryBuilder {
+    private QueryBuilder(){}
     public static Example<Planet> makeQuery(Planet planet) {
-        ExampleMatcher matcher =  ExampleMatcher
-                .matchingAll()
-                .withIgnoreCase()
-                .withIgnoreNullValues();
-        return Example.of(planet, matcher);
+        ExampleMatcher exampleMatcher = ExampleMatcher.matchingAll().withIgnoreCase().withIgnoreNullValues();
+        return Example.of(planet, exampleMatcher);
     }
 }
+
